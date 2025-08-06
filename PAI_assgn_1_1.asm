16478c1129c9893ad86f61442b009d4bc42c24fc
SplineUser <Priyanshusharma2022@gmail.com>
2025-08-06 20:19:09 +0530

    Updated PAI_assgn_1_1.asm

--- PAI_assgn_1_1.asm
+++ PAI_assgn_1_1.asm
@@ -1,5 +1,5 @@
 section .data
-        first_name db "First_Name ", 0
+        first_name db "First_Name ", 10, 0
         last_name db "Last_Name", 0
 
 section .text
@@ -9,7 +9,7 @@ _start:
                 mov eax, 4
                 mov ebx, 1
                 mov ecx, first_name
-                mov edx, 11
+                mov edx, 12
                 int 0x80
 
                 mov eax, 4
